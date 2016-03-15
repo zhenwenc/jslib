ba91ea3c144622c353d4874ddbbad3b517792f0c
Frederick Cai <frederick.cai@gmail.com>
2016-03-15 21:31:19 +1300

    Updates Equals to be interface

--- src/core/Equals.ts
+++ src/core/Equals.ts
@@ -3,19 +3,13 @@
  * in this packages impliments this interface.
  */
 
-export const IS_EQUALS_SENTINEL = '@@__TSFP_EQUALS__@@'
-
 export function canEquals(that: any) {
-  // return !!(that && that[IS_EQUALS_SENTINEL])
   return that && typeof that.equals === 'function'
 }
 
-export abstract class Equals {
-  // IS_EQUALS_SENTINEL
-  get '@@__TSFP_EQUALS__@@'() { return true }
-
+export interface Equals {
   /**
    * The universal equality method, should consider deep equals.
    */
-  abstract equals(that: any): boolean
+  equals(that: any): boolean
 }
