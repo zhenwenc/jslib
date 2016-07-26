e1d174bb1f2dfd5a49410c1d60de74d61646fa39
Frederick Cai <frederick.cai@gmail.com>
2016-07-26 20:44:32 +1200

    Release 0.1.9 - Add custom Error wrapper class

--- src/core/Errors.ts
+++ src/core/Errors.ts
@@ -1,19 +1,21 @@
 
-export class BaseError {
-  constructor () {
-    Error.apply(this, arguments)
+export class BaseError extends Error {
+  constructor (message?: string) {
+    super()
+    this.message = message
+    this.stack = new Error().stack
   }
 }
 BaseError.prototype = new Error()
 
 export class ServiceError extends BaseError {
   constructor(message: string) {
-    super()
+    super(message)
   }
 }
 
 export class ClientError extends BaseError {
   constructor(message: string) {
-    super()
+    super(message)
   }
 }
