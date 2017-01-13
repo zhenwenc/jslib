4cc8ab37a4ca206f19d579f1210d4c73544d607d
Frederick Cai <frederick.cai@gmail.com>
2017-01-13 16:06:44 +1300

    Update config, update Error class

--- src/core/Errors.ts
+++ src/core/Errors.ts
@@ -3,12 +3,11 @@
  */
 
 export class NcError extends Error {
-  constructor (message?: string) {
+  constructor (message: string) {
     super()
     this.message = message
   }
 }
-NcError.prototype = new Error()
 
 export class NcSevere extends NcError {
   constructor(message: string) {
